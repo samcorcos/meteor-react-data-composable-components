8cf08d45acf1abfe147b1f0e65c6618a3bc66368
Sam Corcos <sam@corcos.io>
2015-08-16 07:48:17 +0200

    basic template

--- meteor-react-data.jsx
+++ meteor-react-data.jsx
@@ -2,6 +2,22 @@ MyData = new Mongo.Collection('myData');
 
 if (Meteor.isClient) {
 
+  Meteor.startup(function() {
+    React.render(
+      <Component />,
+      document.getElementById('app')
+    );
+  })
+
+  Component = React.createClass({
+    render() {
+      return (
+        <div>
+          This is where the data goes
+        </div>
+      )
+    }
+  })
 }
 
 if (Meteor.isServer) {
